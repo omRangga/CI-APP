31ab3a5016e3d7bb854d1e6265c1b555b54b417c
Rangga Adi Permana <rangga.163040073@mail.unpas.ac.id>
2019-10-15 17:57:39 +0700

    Commit #4

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -12,7 +12,11 @@ class Mahasiswa extends CI_Controller
     public function index()
     {
         $data['judul'] = 'Daftar Mahasiswa';
-        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
+        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiwa();
+        if ($this->input->post('keyword')) {
+            $data['mahasiswa']=$this->Mahasiswa_model->cariDataMahasiswa();
+           
+        }
         $this->load->view('templates/header', $data);
         $this->load->view('mahasiswa/index', $data);
         $this->load->view('templates/footer');
